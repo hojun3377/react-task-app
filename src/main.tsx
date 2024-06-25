a5b1d9d2cbad7de35c5cc00338207ff36787c038
hojun3377 <hojun3377@gmail.com>
2024-06-25 15:26:45 +0900

    feat: 전역 스타일 생성 및 적용

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 
 import App from './App.tsx';
 import store from './store/index.ts';
-import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
