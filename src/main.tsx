62c0e28b3e984a4c5c56e52a554282a5be6e475d
hojun3377 <hojun3377@gmail.com>
2024-06-25 19:15:29 +0900

    feat: main 페이지 스타일링

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 
 import App from './App.tsx';
 import store from './store/index.ts';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
