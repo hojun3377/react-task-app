8c3d23f0487779ff14e2d20439fa73da00a89364
hojun3377 <hojun3377@gmail.com>
2024-06-25 15:12:24 +0900

    feat: Redux 초기 세팅
    - slice
        - boards: task 모음에 대한 게시판 데이터
        - logger: 활동 기록에 대한 데이터
        - modal: task 상세 기록에 대한 modal 데이터
    - types: 데이터 타입 정의

---
+++ src/store/index.ts
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+import reducer from './reducer/reducer';
+
+const store = configureStore({
+  reducer,
+});
+
+export default store;
---
+++ src/store/slices/boardsSlice.ts
@@ -0,0 +1,63 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { IBoard } from '../../types';
+
+type TBoardsState = {
+  modalActive: boolean;
+  boardArray: IBoard[];
+};
+
+const initialState: TBoardsState = {
+  modalActive: false,
+  boardArray: [
+    {
+      boardId: 'board-0',
+      boardName: '첫 번째 게시물',
+      lists: [
+        {
+          listId: 'list-0',
+          listName: 'List 1',
+          tasks: [
+            {
+              taskId: 'Task-0',
+              taskName: 'Task 1',
+              taskDescription: 'Description',
+              taskOwner: 'Hojun',
+            },
+            {
+              taskId: 'Task-1',
+              taskName: 'Task 2',
+              taskDescription: 'Description',
+              taskOwner: 'Hojun',
+            },
+            {
+              taskId: 'Task-2',
+              taskName: 'Task 3',
+              taskDescription: 'Description',
+              taskOwner: 'Hojun',
+            },
+          ],
+        },
+        {
+          listId: 'list-1',
+          listName: 'List 2',
+          tasks: [
+            {
+              taskId: 'Task-3',
+              taskName: 'Task 4',
+              taskDescription: 'Description',
+              taskOwner: 'Hojun',
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
+
+const boardsSlice = createSlice({
+  name: 'boards',
+  initialState,
+  reducers: {},
+});
+
+export const boardsReducer = boardsSlice.reducer;
