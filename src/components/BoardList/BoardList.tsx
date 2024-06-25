e9ed257e5b0f9b4ca38410b4db550f9db8097511
hojun3377 <hojun3377@gmail.com>
2024-06-25 19:14:55 +0900

    feat: Board List 컴포넌트 구현 및 스타일링

--- src/components/BoardList/BoardList.tsx
+++ src/components/BoardList/BoardList.tsx
@@ -1,7 +1,64 @@
-import React from 'react';
+import React, { FC, useState } from 'react';
+import { FiPlusCircle } from 'react-icons/fi';
+import clsx from 'clsx';
 
-const BoardList = () => {
-  return <div>BoardList</div>;
+import { useTypedSelector } from '../../hooks/redux';
+import SideForm from './SideForm/SideForm';
+import {
+  addButton,
+  addSection,
+  boardItem,
+  boardItemActive,
+  container,
+  title,
+} from './BoardList.css';
+
+type TBoardListProps = {
+  activeBoardId: string;
+  setActiveBoardId: React.Dispatch<React.SetStateAction<string>>;
+};
+
+const BoardList: FC<TBoardListProps> = ({
+  activeBoardId,
+  setActiveBoardId,
+}) => {
+  const { boardArray } = useTypedSelector(state => state.boards);
+  const [isFormOpen, setIsFromOpen] = useState(false);
+
+  return (
+    <div className={container}>
+      <div className={title}>게시판: </div>
+      {boardArray.map((board, index) => (
+        <div
+          key={index}
+          className={clsx(
+            {
+              [boardItemActive]:
+                boardArray.findIndex(b => b.boardId === activeBoardId) ===
+                index,
+            },
+            {
+              [boardItem]:
+                boardArray.findIndex(b => b.boardId === activeBoardId) !==
+                index,
+            }
+          )}
+          onClick={() => setActiveBoardId(boardArray[index].boardId)}>
+          <div>{board.boardName}</div>
+        </div>
+      ))}
+      <div className={addSection}>
+        {isFormOpen ? (
+          <SideForm />
+        ) : (
+          <FiPlusCircle
+            className={addButton}
+            onClick={() => setIsFromOpen(!isFormOpen)}
+          />
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default BoardList;
