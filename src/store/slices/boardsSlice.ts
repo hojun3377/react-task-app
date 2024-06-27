cbd7414a585d565447973c1fa0dfd46280f62413
hojun3377 <hojun3377@gmail.com>
2024-06-27 13:41:22 +0900

    feat: Modal Edit 컴포넌트 구현 및 스타일링

--- src/store/slices/boardsSlice.ts
+++ src/store/slices/boardsSlice.ts
@@ -26,6 +26,12 @@ type TAddTaskAction = {
   task: ITask;
 };
 
+type TDeleteTaskAction = {
+  boardId: string;
+  listId: string;
+  taskId: string;
+};
+
 const initialState: TBoardsState = {
   modalActive: false,
   boardArray: [
@@ -78,16 +84,6 @@ const boardsSlice = createSlice({
   name: 'boards',
   initialState,
   reducers: {
-    addBoard: (state, { payload }: PayloadAction<TAddBoardAction>) => {
-      state.boardArray.push(payload.board);
-    },
-    addList: (state, { payload }: PayloadAction<TAddListAction>) => {
-      state.boardArray.map(board =>
-        board.boardId === payload.boardId
-          ? { ...board, lists: board.lists.push(payload.list) }
-          : board
-      );
-    },
     addTask: (state, { payload }: PayloadAction<TAddTaskAction>) => {
       state.boardArray.map(board =>
         board.boardId === payload.boardId
@@ -102,6 +98,56 @@ const boardsSlice = createSlice({
                   : list
               ),
             }
+          : board
+      );
+    },
+    updateTask: (state, { payload }: PayloadAction<TAddTaskAction>) => {
+      state.boardArray = state.boardArray.map(board =>
+        board.boardId === payload.boardId
+          ? {
+              ...board,
+              lists: board.lists.map(list =>
+                list.listId === payload.listId
+                  ? {
+                      ...list,
+                      tasks: list.tasks.map(task =>
+                        task.taskId === payload.task.taskId
+                          ? payload.task
+                          : task
+                      ),
+                    }
+                  : list
+              ),
+            }
+          : board
+      );
+    },
+    deleteTask: (state, { payload }: PayloadAction<TDeleteTaskAction>) => {
+      state.boardArray = state.boardArray.map(board =>
+        board.boardId === payload.boardId
+          ? {
+              ...board,
+              lists: board.lists.map(list =>
+                list.listId === payload.listId
+                  ? {
+                      ...list,
+                      tasks: list.tasks.filter(
+                        task => task.taskId !== payload.taskId
+                      ),
+                    }
+                  : list
+              ),
+            }
+          : board
+      );
+    },
+    addBoard: (state, { payload }: PayloadAction<TAddBoardAction>) => {
+      state.boardArray.push(payload.board);
+    },
+    addList: (state, { payload }: PayloadAction<TAddListAction>) => {
+      state.boardArray.map(board =>
+        board.boardId === payload.boardId
+          ? { ...board, lists: board.lists.push(payload.list) }
           : board
       );
     },
@@ -121,6 +167,13 @@ const boardsSlice = createSlice({
   },
 });
 
-export const { addBoard, deleteList, setModalActive, addList, addTask } =
-  boardsSlice.actions;
+export const {
+  addBoard,
+  deleteList,
+  setModalActive,
+  addList,
+  addTask,
+  updateTask,
+  deleteTask,
+} = boardsSlice.actions;
 export const boardsReducer = boardsSlice.reducer;
