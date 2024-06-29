dc2192dd6bbff3a8cac579980415bdf52d78c027
hojun3377 <hojun3377@gmail.com>
2024-06-29 22:53:28 +0900

    chore: Firebase 연결 초기 세팅

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyB7sXqeb0-bOCilCsG8rIKhodKpdKtCHrk',
+  authDomain: 'react-task-app-add2d.firebaseapp.com',
+  projectId: 'react-task-app-add2d',
+  storageBucket: 'react-task-app-add2d.appspot.com',
+  messagingSenderId: '333776995191',
+  appId: '1:333776995191:web:4b0f357598b2826a369772',
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
